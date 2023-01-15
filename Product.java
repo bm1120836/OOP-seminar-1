7b3d90490b2a642933a79e628b07be087d8c6baf
BM <bm1120836@yandex.ru>
2023-01-15 21:50:55 +0300

    Product

---
+++ Product.java
@@ -0,0 +1,24 @@
+public class Product {
+    private int price;
+    private String name;
+
+    public Product(int price, String name) {
+        this.price = price;
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("product %s, price %d", name, price);
+    }
+
+    
+}
