50f5607d1ca2c188719ddbc2c830b663ad114b94
BM <bm1120836@yandex.ru>
2023-01-15 21:50:27 +0300

    Raviolli

---
+++ Raviolli.java
@@ -0,0 +1,12 @@
+public class Raviolli extends Product{         // расширяем класс Product
+    private int count;
+    public Raviolli(int price, String name, int count) {
+        super(price, name);
+        this.count = count;
+    }
+    @Override
+    public String toString() {
+        return super.toString() + " " + count;
+    }
+    
+}
